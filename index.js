d80be01d19e33bda5b3bb8b937df491f3c35a516
emm4nz <130589932+emm4nz@users.noreply.github.com>
2023-09-14 15:50:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function changeImg(img1) {
+    var image = document.getElementById('img1');
+    image.src = 'img/images (4).jpg';
+}
+
+function changeImg(img2) {
+    var image = document.getElementById('img2');
+    image.src = 'img/images (3).jpg';
+}
+
+function changeImg(img3) {
+    var image = document.getElementById('img3');
+    image.src = 'img/images (2).jpg';
+}
